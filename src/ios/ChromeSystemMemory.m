e6c0e4846f2e99954bd81ec409910ed159d0658f
Mr. D <uiktiomasfeliz@gmail.com>
2023-03-02 15:11:24 +0100

    Update ChromeSystemMemory.m

--- src/ios/ChromeSystemMemory.m
+++ src/ios/ChromeSystemMemory.m
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import <Cordova/CDVPlugin.h>
 #import <Foundation/Foundation.h>
 #import <mach/mach.h>
 #import <mach/mach_host.h>
